6a80ed51fe79321f975ac4aa4f265ef700021cc7
felixokolo <pheelix.info@gmail.com>
2022-12-20 06:48:01 +0100

    correction

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,3 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import concatArrays from './5-spread-operator.js';
 
-console.log(getBudgetObject(400, 700, 900));
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));

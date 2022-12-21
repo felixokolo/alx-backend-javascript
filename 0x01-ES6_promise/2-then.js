3fd1e958736b3f0ba4768ced76e880dde010e38a
felixokolo <pheelix.info@gmail.com>
2022-12-21 23:40:33 +0100

    additions

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }))
+    .then(() => new Error());
+  console.log('Got a response from the API');
+}

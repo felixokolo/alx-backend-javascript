533e4c25ec581131d725ec52d045db86393f6f16
felixokolo <pheelix.info@gmail.com>
2022-12-21 23:44:43 +0100

    corrections

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,12 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((result) => console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`))
+    .then((res) => {
+      let result = {};
+      for (const resp of res) {
+        result = { ...result, ...resp };
+      }
+      console.log(`${result.body} ${result.firstName} ${result.lastName}`);
+    })
     .catch(() => console.log('Signup system offline'));
 }

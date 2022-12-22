92594821ce54bcf90ade91485771c08c203184dc
felixokolo <pheelix.info@gmail.com>
2022-12-22 21:11:54 +0100

    Addition of docstrings

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,12 @@
+/**
+ * Classroom class
+ */
 export default class ClassRoom {
+  /**
+   * Creates a new ClassRoom class
+   * @param {Number} maxStudentsSize - The maximimum number
+   * of students
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }

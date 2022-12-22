b8972a0a1ee6864f016221a393df1c0e9497b2d0
felixokolo <pheelix.info@gmail.com>
2022-12-22 01:02:22 +0100

    correction to task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
+  return promise.then(() => ({
     status: 200,
     body: 'success',
   }))
     .then(() => new Error());
-  console.log('Got a response from the API');
 }

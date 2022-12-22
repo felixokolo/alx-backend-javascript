faeb9b8c6572205aa2b56ceb17563533851837c5
felixokolo <pheelix.info@gmail.com>
2022-12-22 03:11:41 +0100

    corrections

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (e) {
-    queue.push(e.message);
+    queue.push(`${e.name}: ${e.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }

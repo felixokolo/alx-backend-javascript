97faf79da81254d9c6acf995e63659e1fa1d03c8
felixokolo <pheelix.info@gmail.com>
2022-12-22 04:26:53 +0100

    addition

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -10,8 +10,8 @@ export default async function asyncUploadUser() {
     };
   } else {
     result = {
-      photo: undefined,
-      user: undefined,
+      photo: 'null',
+      user: 'null',
     };
   }
   return result;

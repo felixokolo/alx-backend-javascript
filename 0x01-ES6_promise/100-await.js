163d4a1e4e74b94d4baa8b6e798c70a6bbb05bb5
felixokolo <pheelix.info@gmail.com>
2022-12-22 11:22:22 +0100

    Correction to task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,18 +2,14 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   let result = {};
-  let res;
-  try {
-    res = await Promise.allSettled([uploadPhoto(), createUser()]);
-  } catch (e) {
-    res = [undefined, undefined];
-  } finally {
-    if ((res[0].status === res[1].status) && res[1].status === 'fulfilled') {
-      result = {
-        photo: res[0].value,
-        user: res[1].value,
-      };
-    }
-  }
+  result = await Promise.all([uploadPhoto(), createUser()])
+    .then((val) => ({
+      photo: val[0],
+      user: val[1],
+    }))
+    .catch(() => ({
+      photo: 'null',
+      user: 'null',
+    }));
   return result;
 }

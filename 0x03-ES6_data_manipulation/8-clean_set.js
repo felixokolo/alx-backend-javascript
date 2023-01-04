75b5e7f1fac68cc811ea54ee9e5fa49ca8a6c477
felixokolo <pheelix.info@gmail.com>
2023-01-04 11:23:41 +0100

    correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  const test = 'bon';
-  const re = new RegExp(`/^${test}/`);
-  return Array.from(set).filter((ele) => ele.startsWith(test)).map(
+  if (startString === '') return '';
+  const re = new RegExp(`^${startString}`);
+  return Array.from(set).filter((ele) => ele.startsWith(startString)).map(
     (ele) => ele.replace(re, ''),
-  );
+  ).join('-');
 }

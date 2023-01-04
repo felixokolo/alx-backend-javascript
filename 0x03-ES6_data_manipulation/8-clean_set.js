bc023b045c4853e9ce313b8da53a4b3073a1280b
felixokolo <pheelix.info@gmail.com>
2023-01-04 11:41:41 +0100

    correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string') return '';
   if (startString === '') return '';
-  if (!(startString instanceof String)) return '';
   if (!(set instanceof Set)) return '';
   const re = new RegExp(`^${startString}`);
   return Array.from(set).filter((ele) => ele.startsWith(startString)).map(

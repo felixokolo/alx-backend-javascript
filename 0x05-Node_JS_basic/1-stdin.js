474924b95ad3a5268a0af852e394dfa3a183faeb
felixokolo <pheelix.info@gmail.com>
2023-02-21 22:02:26 +0100

    additions

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdin.setEncoding("utf-8");
-console.log("Welcome to Holberton School, what is your name?");
+process.stdin.setEncoding('utf-8');
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on("readable", () => {
-  user_name = process.stdin.read();
+process.stdin.on('readable', () => {
+  const userName = process.stdin.read();
 
-  if (user_name) {
-    process.stdout.write("Your name is: " + user_name);
+  if (userName) {
+    process.stdout.write(`Your name is: ${userName}`);
   }
 });
 
-process.stdin.on("close", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });

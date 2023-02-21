cfad346d6831cd715f991cb8931a727bcccc8e2c
felixokolo <pheelix.info@gmail.com>
2023-02-21 21:04:14 +0100

    correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
 process.stdin.setEncoding("utf-8");
 console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.on("data", (data) => {
-  console.log("Your name is: " + data);
+process.stdin.on("readable", () => {
+  user_name = process.stdin.read();
+
+  if (user_name) {
+    process.stdout.write("Your name is: " + user_name);
+  }
 });
 
-process.on("beforeExit", () => {
-  console.log("This important software is now closing");
-  process.exit(0);
+process.stdin.on("close", () => {
+  process.stdout.write("This important software is now closing\n");
 });
